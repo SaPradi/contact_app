07249a2d535f9cd747a80f688a26b8db94e710a1
SaPradi <a.garces@globant.com>
2024-07-25 11:11:00 -0500

    fix: regex string no accept acentuados

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,4 +1,3 @@
-
 export function isValidEmail(email:string):boolean {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
@@ -8,7 +7,8 @@ export function isNotEmpty(str:string):boolean {
     const notEmptyRegex = /^.*\S.*$/;
     return notEmptyRegex.test(str.trim());
 }
-export function onlyString(str:string):boolean{
-    const stringRegex =/^[a-zA-Z]+$/;
-    return stringRegex.test(str)
+
+export function onlyString(str: string): boolean {
+    const stringRegex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ]+$/;
+    return stringRegex.test(str);
 }
